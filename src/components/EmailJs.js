b269a304ec05cf130ad10c75d08055c996e5258d
Helena Johansson <helena@klarr.se>
2021-01-14 13:21:14 +0100

    emailjs example finished

--- src/components/EmailJs.js
+++ src/components/EmailJs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 class EmailJs extends React.Component {
   constructor(props) {
@@ -16,11 +17,48 @@ class EmailJs extends React.Component {
     this.setState({ [name]: value });
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+
+    // emailjs: servide id, template id, template params, user id
+
+    let templateParams = {
+      name: name,
+      email: email,
+      message: message,
+    };
+
+    emailjs.send(
+      "<YOUR SERVICE ID>",
+      "<YOUR TEMPLATE ID>",
+      templateParams,
+      "<YOUR USER ID>"
+    );
+
+    console.log(`
+        --SUBMITTING--
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}`);
+
+    this.resetForm();
+  };
+
+  resetForm() {
+    this.setState({
+      name: "",
+      email: "",
+      message: "",
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Email JS Example</h1>
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit} noValidate>
           <div className="form-control">
             <label htmlFor="name">Name: </label>
             <input
@@ -53,6 +91,9 @@ class EmailJs extends React.Component {
               formNoValidate
             />
           </div>
+          <button className="form-btn" type="submit">
+            Send
+          </button>
         </form>
       </div>
     );
